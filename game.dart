aaea9f39730a1e1113473f546f2c398e907f15b9
Nguyen Louis <nguyenlouis172@gmail.com>
2023-07-07 10:11:10 +0200

    add styles in game.dart

--- game.dart
+++ game.dart
@@ -33,39 +33,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Bienvenue'),
+        backgroundColor: Colors.red,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+      body: const Text('Mon application',
+                        style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.red,
+      ))
+                    );
   }
 }
+
