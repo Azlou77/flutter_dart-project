db0955e8b008223933e13249a8493998b9fc34c7
Nguyen Louis <nguyenlouis172@gmail.com>
2023-07-07 12:02:42 +0200

    update fields forms

--- exercices/app.dart
+++ exercices/app.dart
@@ -5,3 +5,43 @@
     - 1 password field
     - 1 button  */
 
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          const Images(
+          image: NetworkImage('https://s2.qwant.com/thumbr/474x266/c/8/f187c10a03093415719d2feb55fb7a6992a3be66735c4c4e7ec3a9bd380455/th.jpg?u=https%3A%2F%2Ftse2.explicit.bing.net%2Fth%3Fid%3DOIP.fqZ9-PPqcG_cm0k3JfoINQHaEK%26pid%3DApi&q=0&b=1&p=0&a=0'),
+          ),
+          title: Text('Bienvenue'),
+          backgroundColor: Colors.red,
+        ),
+        body: 
+        // Add Text
+        const TextField(
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Louis Nguyen',
+          ),
+        ), 
+        // Add email field
+        const TextFormField(
+          decoration: InputDecoration(
+            hintText: 'Entrez votre email',
+          ),
+        ),
+
+        // Add button
+        ElevatedButton(
+          onPressed: () {
+              print("j'ai cliqué");
+          },
+          child: const Text('Cliquez ici'),
+          ),
+      );
+      }
+  }
+
